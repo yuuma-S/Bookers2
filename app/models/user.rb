4d92f5ef0f615ccc85cacef9b5f01f107d319907
yuuma-S <y.sakurai2206@gmail.com>
2020-01-13 11:36:32 +0000

    model & controller & model & route add model asociation  config

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,17 @@ class User < ApplicationRecord
 	has_many :favorites
 	has_many :comments
 
+	has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+	has_many :followings, through: :active_relationships, source: :follower
+
+	has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  	has_many :followers, through: :passive_relationships, source: :following
+
+  	def followed_by?(user)
+    # 今自分(引数のuser)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
+    passive_relationships.find_by(following_id: user.id).present?
+  	end
+
 	validates :name,    length: { in: 2..20 }
 	validates :introduction,    length: { maximum: 50 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_065842) do
+ActiveRecord::Schema.define(version: 2020_01_13_104822) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,6 +31,13 @@ ActiveRecord::Schema.define(version: 2020_01_13_065842) do
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "following_id"
+    t.integer "follower_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
