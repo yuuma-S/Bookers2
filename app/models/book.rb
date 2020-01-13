17be552fa809cacba8366e67706512773d9acb7b
yuuma-S <y.sakurai2206@gmail.com>
2020-01-13 01:22:09 +0000

    favorite function add

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,12 @@
 class Book < ApplicationRecord
 	belongs_to :user
+	has_many :favorites
 	validates :title, presence: true
 	validates :body, presence: true
 	validates :body, length: { maximum: 200 }
+
+	def favorited_by?(user)
+        favorites.where(user_id: user.id).exists?
+    end
 
 end
