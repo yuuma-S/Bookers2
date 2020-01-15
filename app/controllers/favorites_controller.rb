e5421df5f10ffe73634ee14cc09a44b5eead5a1b
yuuma-S <y.sakurai2206@gmail.com>
2020-01-15 10:13:56 +0000

    favorite Ajax

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     # こう記述することで、「current_userに関連したFavoriteクラスの新しいインスタンス」が作成可能。
     # つまり、favorite.user_id = current_user.idが済んだ状態で生成されている。
     # buildはnewと同じ意味で、アソシエーションしながらインスタンスをnewする時に形式的に使われる。
-	current_user.favorites.create(book_id: params[:book_id])
-    redirect_to request.referer
+    current_user.favorites.create(book_id: params[:book_id])
+
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+
     favorite = Favorite.find_by(book_id: params[:book_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to request.referer
+
   end
 end
